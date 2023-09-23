7b8c691a22783a0e6a3d055ba06c3b972ee5e98d
Aditi Patel <67344657+AditiPatelN@users.noreply.github.com>
2023-09-22 22:27:34 -0400

    Add files via upload

---
+++ Aditi_Patel_p1/cpu/vcpu_scheduler.c
@@ -0,0 +1,294 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <libvirt/libvirt.h>
+#include <math.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <signal.h>
+#define MIN(a, b) ((a) < (b) ? a : b)
+#define MAX(a, b) ((a) > (b) ? a : b)
+
+int is_exit = 0; // DO NOT MODIFY THIS VARIABLE
+// Added global variables for persistent code
+typedef struct
+{
+	virDomainPtr domain_id;
+	int vcpu_id;
+	unsigned char cpumap;
+	double utilization;
+	int assigned_pcpu;
+} vcpuMapping;
+int num_pcpu = 4;		   // will dynamically assign in CPUScheduler()
+int current_pcpu = 0;	   // all vCPUs are pinned to pCPU 0 in the beginning
+unsigned long *vcpu_times; // Array to track vCPU times for each domain
+static int mem_allocated = 0;
+static int first_iteration = 1;
+// have global variables that keeps track of the time- to get the cpu percentage
+void CPUScheduler(virConnectPtr conn, int interval);
+
+/*
+DO NOT CHANGE THE FOLLOWING FUNCTION
+*/
+void signal_callback_handler()
+{
+	printf("Caught Signal");
+	is_exit = 1;
+}
+
+/*
+DO NOT CHANGE THE FOLLOWING FUNCTION
+*/
+int main(int argc, char *argv[])
+{
+	virConnectPtr conn;
+
+	if (argc != 2)
+	{
+		printf("Incorrect number of arguments\n");
+		return 0;
+	}
+
+	// Gets the interval passes as a command line argument and sets it as the STATS_PERIOD for collection of balloon memory statistics of the domains
+	int interval = atoi(argv[1]);
+
+	conn = virConnectOpen("qemu:///system");
+	if (conn == NULL)
+	{
+		fprintf(stderr, "Failed to open connection\n");
+		return 1;
+	}
+
+	// Get the total number of pCpus in the host
+	signal(SIGINT, signal_callback_handler);
+
+	while (!is_exit)
+	// Run the CpuScheduler function that checks the CPU Usage and sets the pin at an interval of "interval" seconds
+	{
+		CPUScheduler(conn, interval);
+		sleep(interval);
+	}
+
+	// Closing the connection
+	virConnectClose(conn);
+	// return 0;
+}
+
+/* COMPLETE THE IMPLEMENTATION */
+void CPUScheduler(virConnectPtr conn, int interval)
+{
+	// domain pointer variable
+	//  virDomainPtr domain;
+	//  int stats;
+	// connects to the hypervisor
+	conn = virConnectOpen("qemu:///system");
+	if (conn == NULL)
+	{
+		fprintf(stderr, "Failed to connect to the hypervisor");
+		// return 1;
+	}
+	// Declare variables for node and domain
+	virNodeInfo node_info;
+	virDomainInfo domain_info;
+	virDomainPtr domain;
+	int maplen;
+	unsigned char cpumaps;
+	// check if there is uneven workload
+	int is_uneven = 0;
+	// get the number of pCPUs-global variable
+	if (virNodeGetInfo(conn, &node_info) < 0)
+	{
+		fprintf(stderr, "Failed to get node information\n");
+	}
+	else
+	{
+		num_pcpu = node_info.cpus;
+		maplen = VIR_CPU_MAPLEN(num_pcpu);
+		printf("Number of pCPU on the host: %d\n", num_pcpu);
+	}
+	// get the number of active domain(VM)
+	int num_domain = virConnectNumOfDomains(conn); // number of active domains/VMs
+	if (num_domain > 0)
+	{
+		vcpuMapping vcpu_mappings[num_domain];
+		int *activeDomains = (int *)malloc(sizeof(int) * num_domain);
+		int numDomainList = virConnectListDomains(conn, activeDomains, num_domain);
+		if (numDomainList < 0)
+		{
+			fprintf(stderr, "Failed to get the list of active domains\n");
+		}
+		else
+		{
+			printf("Active Virtual Machines: \n");
+			// initially assign vcpu times to 0
+			if (!mem_allocated)
+			{
+
+				for (int a = 0; a < num_domain; a++)
+				{
+					// vcpu_mappings[a].vcpu_time = 0;
+					vcpu_mappings[a].utilization = 0;
+				}
+				vcpu_times = (unsigned long *)calloc(num_domain, sizeof(unsigned long));
+				mem_allocated = 1;
+			}
+
+			for (int i = 0; i < numDomainList; i++)
+			{
+
+				domain = virDomainLookupByID(conn, activeDomains[i]);
+				if (domain == NULL)
+				{
+					fprintf(stderr, "Failed to find domain by ID");
+				}
+				else
+				{
+					vcpu_mappings[i].domain_id = domain; // domain id
+					vcpu_mappings[i].vcpu_id = 0;		 // all domains only have 1 vcpu
+					if (virDomainGetInfo(domain, &domain_info) == 0)
+					{
+						unsigned long new_time = domain_info.cpuTime; // new time for domain
+						printf("new vcpu time %lu\n", new_time);
+						unsigned long time_diff = new_time - vcpu_times[i];
+						printf("new vcpu time %lu\n", time_diff);
+						double vcpu_utilization = ((double)time_diff / (interval * 1000000000ULL)) * 100; // should be percentage
+						vcpu_mappings[i].utilization = vcpu_utilization;								  // storing utilization for each domain to take average
+						printf("vCPU utilization for domain %s: %.2f%%\n", virDomainGetName(domain), vcpu_utilization);
+
+						// retrieve affinity
+						// unsigned char cpumaps;
+						virVcpuInfoPtr vcpu_info = (virVcpuInfoPtr)malloc(sizeof(virVcpuInfo));
+						if (virDomainGetVcpus(domain, vcpu_info, 1, &cpumaps, maplen) == maplen)
+						{
+							vcpu_mappings[i].cpumap = cpumaps;
+						}
+						else
+						{
+							fprintf(stderr, "Failed to get vCPU info for domain %d\n", i);
+						}
+
+						// int pcpu_assigned = current_pcpu; // pin to least busy pcpu
+
+						printf("Maplen= %d\n", maplen);
+						// naive RR algorithm
+						// int index = cpumaps_size - pcpu_assigned - 1;
+						// cpumaps = 1 << pcpu_assigned; // does this allow us to specify the pCPU to pin to?
+						// int cpumaps_size = sizeof(cpumaps);
+						printf("size of cpumaps: %lu\n", sizeof(unsigned char));
+
+						// current_pcpu = (current_pcpu + 1) % num_pcpu;
+						// pin the current vcpu to selected pcpu
+						// if (virDomainPinVcpu(domain, 0, &cpumaps, maplen) != 0)
+						// {
+						// 	fprintf(stderr, "Failed to pin the vCPU: %d of domain %s to pCpu: %d\n", 0, virDomainGetName(domain), pcpu_assigned);
+						// }
+						// else
+						// {
+						// 	printf("Successfully pinned the vCPU: %d of domain %s to pCpu: %d\n", 0, virDomainGetName(domain), pcpu_assigned);
+						// }
+						vcpu_times[i] = new_time;
+					}
+					else
+					{
+						fprintf(stderr, "virDomainGetInfo != 0");
+					}
+				}
+			}
+			// Go through the vcpu_utilization here to determine uneven load
+			// compute pcpu_utilization only when uneven load - or get priority queue
+			for (int i = 0; i < num_domain; i++)
+			{
+				int next_index = (i + 1) % num_domain;
+				double diff_utilization = fabs(vcpu_mappings[i].utilization - vcpu_mappings[next_index].utilization);
+				printf("Inside is_uneven for loop, diff_utilization = %.2f\n", diff_utilization);
+				if (diff_utilization > 20)
+				{
+					is_uneven = 1;
+					break;
+				}
+			}
+			if (first_iteration)
+			{
+				// if first iteration- run RR, else run RR only when uneven workload
+				first_iteration = 0;
+				printf("Using the round robin for the first iteration\n");
+				// run the naive round robin
+				for (int i = 0; i < num_domain; i++)
+				{
+					vcpu_mappings[i].assigned_pcpu = current_pcpu; // pin to least busy pcpu
+					current_pcpu = (current_pcpu + 1) % num_pcpu;
+					// after getting pcpu_assigned, pin them
+					cpumaps = 1 << vcpu_mappings[i].assigned_pcpu; // does this allow us to specify the pCPU to pin to?
+					//  pin the current vcpu to selected pcpu
+					if (virDomainPinVcpu(vcpu_mappings[i].domain_id, 0, &cpumaps, maplen) != 0)
+					{
+						fprintf(stderr, "Failed to pin the vCPU: %d of domain %s to pCpu: %d\n", 0, virDomainGetName(vcpu_mappings[i].domain_id), vcpu_mappings[i].assigned_pcpu);
+					}
+					else
+					{
+						printf("Successfully pinned the vCPU: %d of domain %s to pCpu: %d\n", 0, virDomainGetName(vcpu_mappings[i].domain_id), vcpu_mappings[i].assigned_pcpu);
+					}
+				}
+			}
+			if (is_uneven & !first_iteration)
+			{
+
+				// sort the vcpu_mapping array in high to low utilization
+
+				printf("Running modified algorithm\n");
+				// run the difference algorithm
+				for (int i = 0; i < num_domain - 1; i++)
+				{
+					for (int j = 0; j < num_domain - i - 1; j++)
+					{
+						if (vcpu_mappings[j].utilization < vcpu_mappings[j + 1].utilization)
+						{
+							vcpuMapping temp = vcpu_mappings[j];
+							vcpu_mappings[j] = vcpu_mappings[j + 1];
+							vcpu_mappings[j + 1] = temp;
+						}
+					}
+				}
+				printf("Using the round robin\n");
+				// run the naive round robin
+				for (int i = 0; i < num_domain; i++)
+				{
+					vcpu_mappings[i].assigned_pcpu = current_pcpu; // pin to least busy pcpu
+					current_pcpu = (current_pcpu + 1) % num_pcpu;
+					// after getting pcpu_assigned, pin them
+					cpumaps = 1 << vcpu_mappings[i].assigned_pcpu; // does this allow us to specify the pCPU to pin to?
+					//  pin the current vcpu to selected pcpu
+					if (virDomainPinVcpu(vcpu_mappings[i].domain_id, 0, &cpumaps, maplen) != 0)
+					{
+						fprintf(stderr, "Failed to pin the vCPU: %d of domain %s to pCpu: %d\n", 0, virDomainGetName(vcpu_mappings[i].domain_id), vcpu_mappings[i].assigned_pcpu);
+					}
+					else
+					{
+						printf("Successfully pinned the vCPU: %d of domain %s to pCpu: %d\n", 0, virDomainGetName(vcpu_mappings[i].domain_id), vcpu_mappings[i].assigned_pcpu);
+					}
+				}
+			}
+			// else
+			// {
+			// first_iteration = 0;
+			// printf("Using the round robin\n");
+			// // run the naive round robin
+			// for (int i = 0; i < num_domain; i++)
+			// {
+			// 	vcpu_mappings[i].assigned_pcpu = current_pcpu; // pin to least busy pcpu
+			// 	current_pcpu = (current_pcpu + 1) % num_pcpu;
+			// 	// after getting pcpu_assigned, pin them
+			// 	cpumaps = 1 << vcpu_mappings[i].assigned_pcpu; // does this allow us to specify the pCPU to pin to?
+			// 	//  pin the current vcpu to selected pcpu
+			// 	if (virDomainPinVcpu(vcpu_mappings[i].domain_id, 0, &cpumaps, maplen) != 0)
+			// 	{
+			// 		fprintf(stderr, "Failed to pin the vCPU: %d of domain %s to pCpu: %d\n", 0, virDomainGetName(vcpu_mappings[i].domain_id), vcpu_mappings[i].assigned_pcpu);
+			// 	}
+			// 	else
+			// 	{
+			// 		printf("Successfully pinned the vCPU: %d of domain %s to pCpu: %d\n", 0, virDomainGetName(vcpu_mappings[i].domain_id), vcpu_mappings[i].assigned_pcpu);
+			// 	}
+			// }
+		}
+	}
+}
---
+++ Aditi_Patel_p1/memory/memory_coordinator.c
@@ -0,0 +1,269 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <libvirt/libvirt.h>
+#include <math.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <signal.h>
+#define MIN(a, b) ((a) < (b) ? a : b)
+#define MAX(a, b) ((a) > (b) ? a : b)
+
+int is_exit = 0;  // DO NOT MODIFY THE VARIABLE
+double *vcpu_mem; // Array to track vCPU memory for each domain
+static int mem_allocated = 0;
+
+void MemoryScheduler(virConnectPtr conn, int interval);
+
+/*
+DO NOT CHANGE THE FOLLOWING FUNCTION
+*/
+void signal_callback_handler()
+{
+	printf("Caught Signal");
+	is_exit = 1;
+}
+
+/*
+DO NOT CHANGE THE FOLLOWING FUNCTION
+*/
+int main(int argc, char *argv[])
+{
+	virConnectPtr conn;
+
+	if (argc != 2)
+	{
+		printf("Incorrect number of arguments\n");
+		return 0;
+	}
+
+	// Gets the interval passes as a command line argument and sets it as the STATS_PERIOD for collection of balloon memory statistics of the domains
+	int interval = atoi(argv[1]);
+
+	conn = virConnectOpen("qemu:///system");
+	if (conn == NULL)
+	{
+		fprintf(stderr, "Failed to open connection\n");
+		return 1;
+	}
+
+	signal(SIGINT, signal_callback_handler);
+
+	while (!is_exit)
+	{
+		// Calls the MemoryScheduler function after every 'interval' seconds
+		MemoryScheduler(conn, interval);
+		sleep(interval);
+	}
+
+	// Close the connection
+	virConnectClose(conn);
+	return 0;
+}
+
+/*
+COMPLETE THE IMPLEMENTATION
+*/
+void MemoryScheduler(virConnectPtr conn, int interval)
+{
+	conn = virConnectOpen("qemu:///system");
+	if (conn == NULL)
+	{
+		fprintf(stderr, "Failed to connect to the hypervisor");
+		// return 1;
+	}
+	// Declare variables for node and domain
+	virNodeInfo node_info;
+	virDomainInfo domain_info;
+	virDomainPtr domain;
+	double threshold = 0.85; // 85% is out threshold
+	double physical_available_mem;
+	// get the number of pCPUs-global variable
+	if (virNodeGetInfo(conn, &node_info) < 0)
+	{
+		fprintf(stderr, "Failed to get node information\n");
+	}
+	else
+	{
+		physical_available_mem = (node_info.memory / 1024); // MB
+		// double physical_max = (node_info.maxMem / 1024);
+		printf("Physical total available memory = : %f\n", physical_available_mem);
+		// printf("Physical max available memory = : %f\n", physical_max);
+	}
+	// get the number of active domain(VM)
+	int num_domain = virConnectNumOfDomains(conn); // number of active domains/VMs
+	if (num_domain > 0)
+	{
+		int *activeDomains = (int *)malloc(sizeof(int) * num_domain);
+		int numDomainList = virConnectListDomains(conn, activeDomains, num_domain);
+		if (numDomainList < 0)
+		{
+			fprintf(stderr, "Failed to get the list of active domains\n");
+		}
+		else
+		{
+			// initially assign memory allocated to 0
+			if (!mem_allocated)
+			{
+				vcpu_mem = (double *)calloc(num_domain, sizeof(double));
+				mem_allocated = 1;
+			}
+			printf("Active Virtual Machines: \n");
+			// memory_coordinator
+			for (int i = 0; i < num_domain; i++)
+			{
+				domain = virDomainLookupByID(conn, activeDomains[i]);
+				if (domain == NULL)
+				{
+					fprintf(stderr, "Failed to find domain with ID %d\n", activeDomains[i]);
+				}
+				else
+				{
+					if (virDomainGetInfo(domain, &domain_info) < 0)
+					{
+						fprintf(stderr, "Failed to goet doman info\n");
+					}
+					else
+					{
+						double domain_max_mem = (double)(virDomainGetMaxMemory(domain)) / 1024; // MB
+						double unused_mem;
+						double balloon_usable; // How much the balloon can be inflated without pushing the guest system to swap, corresponds to 'Available' in /proc/meminfo
+
+						printf("Max memory for domain %s = %f\n", virDomainGetName(domain), domain_max_mem);
+						double threshold_memory = (unsigned long)(threshold * domain_max_mem);
+						printf("Threshold memory for domain %s = %f\n", virDomainGetName(domain), threshold_memory);
+
+						// adjust memory allocation
+						virDomainMemoryStatPtr stats = calloc(VIR_DOMAIN_MEMORY_STAT_NR, sizeof(*stats));
+						int mem_stats = virDomainMemoryStats(domain, stats, VIR_DOMAIN_MEMORY_STAT_NR, 0);
+						if (mem_stats < 0)
+						{
+							fprintf(stderr, "Failed to get memory stats\n");
+						}
+						else
+						{
+							double mem_used = (domain_info.memory) / 1024;
+							printf("MEMORY USED from domain_info = %f\n", mem_used);
+							// int rss_index = VIR_DOMAIN_MEMORY_STAT_RSS;
+							// if (rss_index >= 0 && rss_index < VIR_DOMAIN_MEMORY_STAT_NR)
+							// {
+							// 	printf("inside the if statement for mem_use\n");
+
+							// 	mem_used = (stats[rss_index].val) / 1024; // new mem over interval- MB
+							// 	printf("memory used from VirDomainMemoryStat for domain %s = %f\n", virDomainGetName(domain), mem_used);
+							// }
+							for (int j = 0; j < mem_stats; j++)
+							{
+								if (stats[j].tag == VIR_DOMAIN_MEMORY_STAT_UNUSED)
+								{
+									unused_mem = (stats[j].val) / 1024; // in MB
+								}
+								else if (stats[j].tag == VIR_DOMAIN_MEMORY_STAT_USABLE)
+								{
+									balloon_usable = (stats[j].val) / 1024;
+								}
+							}
+
+							double mem_diff = mem_used - vcpu_mem[i];
+							printf("memory difference for domain %s = %f\n", virDomainGetName(domain), mem_diff);
+							double mem_utilization = ((mem_diff) / (interval * 1000)) * 100; // convert the diff to MB - interval in ms
+							printf("memory utilization for domain %s = %.2f%%\n", virDomainGetName(domain), mem_utilization);
+							vcpu_mem[i] = mem_used;
+							if (unused_mem == 0)
+							{
+								printf("Inside the unused for loop");
+								unused_mem = domain_max_mem - mem_used;
+							}
+							// unused_mem = domain_max_mem - mem_used;
+							printf("ballon memory for domain %s = %f\n", virDomainGetName(domain), balloon_usable);
+							printf("unused memory for domain %s = %f\n", virDomainGetName(domain), unused_mem);
+							double unused_threshold = (0.4 * mem_used); // 40% * max_used
+							printf("unused threshold percentage for domain %s = %f\n", virDomainGetName(domain), unused_threshold);
+							// Allocation when unused <= 100MB; make sure allocated memory to VM > 2048MB
+							// Decallocation when unused > 40% maxMemory or if used == maxMemory
+							// double new_mem_allocation;
+							if (unused_mem <= 150 && mem_used != domain_max_mem)
+							{ // or 100
+								printf("Allocating memory\n");
+								double new_mem_allocation = mem_used + 75; // give memory in gradually
+								virDomainSetMemory(domain, new_mem_allocation);
+								printf("Added memory for domain %s to %f MB\n", virDomainGetName(domain), new_mem_allocation);
+							}
+							else if ((mem_used == domain_max_mem) || (unused_mem > 200))
+							{
+								printf("Deallocating memory\n");
+								double new_mem_allocation = domain_max_mem - 75; // gradually decreasing
+								virDomainSetMemory(domain, new_mem_allocation);
+								printf("Reduced memory for domain %s to %f MB\n", virDomainGetName(domain), new_mem_allocation);
+							}
+							else
+							{
+								printf("No change in memory\n");
+							}
+
+							// virDomainSetMemory(domain, new_mem_allocation);
+
+							// double unused_percentage = (double)(unused_mem / domain_max_mem) * 100;
+							// printf("Unused percentage for domain %s = %.2f%%\n", virDomainGetName(domain), unused_percentage);
+							// if (unused_mem > unused_threshold)
+							// {
+							// 	unsigned long reduction_memory = (unsigned long)(0.6 * domain_max_mem); // take away 60% memory back
+							// 	unsigned long new_mem_allocation = domain_max_mem - reduction_memory;
+							// 	virDomainSetMemory(domain, new_mem_allocation);
+							// 	printf("Reduced memory for domain %s to %lu bytes\n", virDomainGetName(domain), new_mem_allocation);
+							// }
+							// // else if ((mem_utilization < threshold_memory) || (mem_used == domain_max_mem)) // mem_used or utilization
+							// else if (mem_used == domain_max_mem) // mem_used or utilization
+
+							// {
+							// 	// Calculate the memory needed to reach threshold (20% above current memory usage or to the threshold, whichever is smaller)
+							// 	unsigned long reduction_memory = (unsigned long)(0.2 * mem_used);
+							// 	unsigned long new_memory_allocation = domain_max_mem - reduction_memory;
+							// 	printf("NEW MEMORY ALLOCATION for domain %s = %lu\n", virDomainGetName(domain), new_memory_allocation);
+							// 	unsigned long min_memory = 256 * 1024; // min memory is 512MB or 256
+							// 	if (new_memory_allocation < mem_used)
+							// 	{
+							// 		printf("new memory < mem_used\n");
+							// 		new_memory_allocation = mem_used + buffer;
+							// 	}
+							// 	else if (new_memory_allocation < min_memory)
+							// 	{
+							// 		printf("new memory < min_memory\n");
+							// 		new_memory_allocation = min_memory;
+							// 	}
+							// 	else if (new_memory_allocation > domain_max_mem)
+							// 	{
+							// 		printf("new memory > domain_max_mem\n");
+							// 		new_memory_allocation = domain_max_mem;
+							// 	}
+							// 	else
+							// 	{
+							// 		printf("didn't pass any if statements\n");
+							// 		new_memory_allocation = new_memory_allocation;
+							// 	}
+							// 	// check if there is enough physical memory available
+							// 	// if (new_memory_allocation <= physical_available_mem)
+							// 	// { // LOOK INTO THIS
+							// 	printf("NEW MEMORY ALLOCATION for domain %s after if statement = %lu\n", virDomainGetName(domain), new_memory_allocation);
+							// 	virDomainSetMemory(domain, new_memory_allocation);
+							// 	printf("Reduced memory for domain %s to %lu bytes\n", virDomainGetName(domain), new_memory_allocation);
+							// 	// }
+							// 	// else
+							// 	// {
+							// 	// 	printf("Not enough physical memory to reduce for domain %s\n", virDomainGetName(domain));
+							// 	// }
+							// }
+							// else if (unused_mem < (0.20 * domain_max_mem))
+							// {
+							// 	// you need more memory for the VM to deflate the balloon - assign max memory
+							// 	unsigned long new_mem_alloc = mem_used + (0.10 * domain_max_mem);
+							// 	virDomainSetMemory(domain, new_mem_alloc);
+							// 	printf("Increased memory to  %s to %lu bytes\n", virDomainGetName(domain), new_mem_alloc);
+							// }
+						}
+					}
+				}
+			}
+		}
+	}
+}
